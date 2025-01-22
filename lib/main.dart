e984fbbd0b4de8b8b3ae2bccf017040de0808f7f
User PC <daniilbidenko5@gmail.com>
2025-01-22 14:34:09 +0600

    Создал проект, создал все функции, создал пока пустые функции, создал обьект и начал выводить все элементы по отдельности

---
+++ lib/main.dart
@@ -0,0 +1,169 @@
+import 'dart:convert';
+
+void main(){
+final String name = '';
+final String place = '';
+final String familie = '';
+final String marka = '';
+final String kolonka = '';
+final String router = '';
+final String wifi = '';
+final String wargan = '';
+final String popit = '';
+final String marker = '';
+
+final int age = 20;
+final int matematika = 20;
+final int geometry = 20;
+final int marki = 20;
+final int familyage = 20;
+final int classroom = 20;
+final int mamaage = 20;
+final int papaage= 20;
+final int sisterage = 20;
+final int tanteage = 20;
+
+final double room = 20.1;
+final double cm = 20.1;
+final double dm = 20.1;
+final double km = 20.1;
+final double m = 20.1;
+final double rooms = 20.1;
+final double multi = 20.1;
+final double spases = 20.1;
+final double doubles = 20.1;
+final double arms = 20.1;
+
+final bool tf = true;
+final bool yes = false;
+final bool no = true;
+final bool sosal = true;
+final bool da = true;
+final bool net = true;
+final bool kakaha = true;
+final bool danilgay = true;
+final bool mama = true;
+final bool papa = true;
+
+final List massive = [arms,age,da,geometry,spases];
+final List massiveone = [kakaha,net];
+final List massivetwo = [danilgay,da];
+final List massivetrhee = [familyage,familie];
+final List massivefour = [doubles,sisterage];
+final List massivefive = [dm,geometry];
+final List massivesix = [tf,tanteage];
+final List massiveseven = [m,mama];
+final List massivenine = [mamaage,multi];
+final List massivetine = [sosal,no];
+
+
+summary () {
+  return arms + m;
+}
+
+multiplication () {
+  return spases * arms * doubles;
+}
+
+daniilday () {
+  
+}
+
+sosali () {
+  
+}
+
+comprasion () {
+  
+}
+
+changename () {
+  
+}
+
+comprasiondaniils () {
+  
+}
+
+for (int i = 0;i < massive.length; i++) {
+  
+}
+
+for (int i = massivenine.length; i >= 0; i--) {
+  
+}
+
+for (int i = massivenine.length; i >= 0; i--) {
+  
+}
+
+final Map objekct = {
+1:massivetine,
+'famale': m,
+45: {
+  20.223: 'dauck',
+  true: tf,
+  44: {
+    65: {
+      78: {
+        25: {
+          1: {
+            90: {
+              234234: 'soska',
+              'wtf': 90.2,
+              52: {
+                'dacha': arms,
+                 sisterage: {
+                  false: dm,
+                }
+              }
+            }
+          },
+          20.2: true,
+        }
+      }
+    },
+    'jony': [cm,spases]
+  }
+},
+[name,age]: massivesix,
+
+};
+
+// print(objekct[1]);
+// print(objekct['famale']);
+// print(objekct[45]);
+// print(objekct[45][20.223]);
+// print(objekct[45][true]);
+// print(objekct[45][44]);
+// print(objekct[45][44]['jony']);
+// print(objekct[45][44][65][78][25][20.2]);
+// print(objekct[45][44][65][78][25][1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+// print(objekct[1]);
+ 
+
+
+}
